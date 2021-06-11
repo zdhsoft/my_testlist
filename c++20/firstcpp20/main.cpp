4f837ab4cfe75793af59ac8a03b93f305c75c23d
zdhsoft <donghua@shubidatech.com>
2021-06-11 14:13:50 +0800

    实现C++concent的测试

--- c++20/firstcpp20/main.cpp
+++ c++20/firstcpp20/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>;
+#include "module01.h";
 using namespace std;
 int main(int argc, char* argv[]) {
+
+	int a = 9990'9909;
+
 	cout << "hello world for C++ 20" << endl;
+	cout << a << endl;
+	cout << "-------------------------------- test concept" << endl;
+	testConcept();
+	cout << "-------------------------------- test concept" << endl;
 	return 0;
+
 }
---
+++ c++20/firstcpp20/module01.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef _MODULE01_H_
+#define _MODULE01_H_
+int testConcept();
+#endif
