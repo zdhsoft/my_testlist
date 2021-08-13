5ee7d6feede7bdb4eeb33ae84720181f7d8784d3
zdhsoft <donghua@shubidatech.com>
2021-08-13 15:23:01 +0800

    增加我的第一个electron例子

---
+++ javascript/04_electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+/*
+ window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
+*/
