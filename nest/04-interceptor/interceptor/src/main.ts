11111437762db6554b2f7039fdabef23e853635e
zdhsoft <zdhsoft@163.com>
2021-11-16 23:41:26 +0800

    增加拦载实现

--- nest/04-interceptor/interceptor/src/main.ts
+++ nest/04-interceptor/interceptor/src/main.ts
@@ -1,8 +1,15 @@
+import './log4js';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { getLogger } from 'xmcommon';
+import { NestLogger } from './nest.logger';
+import { RequestInterceptor } from './request.interceptor';
+const log = getLogger(__filename);
+log.info('程序开始启动...');
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule, { logger: new NestLogger() });
+    app.useGlobalInterceptors(new RequestInterceptor());
+    await app.listen(3000);
+    log.info('开始侦听:3000...')
 }
 bootstrap();
---
+++ nest/04-interceptor/interceptor/src/request.interceptor.ts
@@ -0,0 +1,29 @@
+/*
+https://docs.nestjs.com/interceptors#interceptors
+*/
+
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { getLogger } from 'xmcommon';
+
+const log = getLogger(__filename);
+let requestSeq = 0;
+
+@Injectable()
+export class RequestInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+
+        const start = Date.now(); // 请求开始时间
+        const host = context.switchToHttp();
+        const request = host.getRequest<Request>();
+        const seq = requestSeq ++;
+        const urlInfo = `${request.method} ${request.url}`;
+        log.info(`[${seq}]==> ${urlInfo}`);
+        return next
+            .handle()
+            .pipe(
+                tap(() => log.info(`[${seq}]<== ${urlInfo} ${Date.now() - start} ms`)),
+            );
+    }
+}
