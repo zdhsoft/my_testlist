384c6d05c5195f55e03ba52433c7e77b97163b19
zdhsoft <zdhsoft@163.com>
2021-11-17 23:55:37 +0800

    完成文件上传实例。

--- nest/04-interceptor/interceptor/src/app.module.ts
+++ nest/04-interceptor/interceptor/src/app.module.ts
@@ -1,11 +1,13 @@
-import { AccountModule } from './account/account.module';
+import { UploadController } from './upload/upload.controller';
+import { UploadService } from './upload/upload.service';
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [AccountModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [],
+    controllers: [UploadController, AppController],
+    providers: [UploadService, AppService],
 })
 export class AppModule {}
---
+++ nest/04-interceptor/interceptor/src/upload/upload.controller.ts
@@ -0,0 +1,28 @@
+/*
+https://docs.nestjs.com/controllers#controllers
+*/
+
+import {
+    Body,
+    Controller,
+    Post,
+    UploadedFile,
+    UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { XCommonRet } from 'xmcommon';
+
+@Controller('upload')
+export class UploadController {
+    @Post('up')
+    @UseInterceptors(FileInterceptor('file'))
+    up(@UploadedFile() file, @Body() body) {
+        const r = new XCommonRet<number>();
+        do {
+            //
+            console.log(file);
+            console.log(body);
+        } while (false);
+        return r.toJSON();
+    }
+}
