906ba6a43f4b0496cedce76ea7a557dc9b0f1ba8
zdhsoft <zdhsoft@163.com>
2021-11-18 14:55:40 +0800

    实现文件上传和下载。

--- nest/04-interceptor/interceptor/src/upload/upload.controller.ts
+++ nest/04-interceptor/interceptor/src/upload/upload.controller.ts
@@ -6,23 +6,54 @@ import {
     Body,
     Controller,
     Post,
+    Res,
     UploadedFile,
     UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { XCommonRet } from 'xmcommon';
+import { Response } from 'express';
 
 @Controller('upload')
 export class UploadController {
     @Post('up')
     @UseInterceptors(FileInterceptor('file'))
-    up(@UploadedFile() file: Express.Multer.File, @Body() body) {
+    public up(@UploadedFile() file: Express.Multer.File, @Body() body) {
         const r = new XCommonRet<number>();
         do {
             //
             console.log(file);
             console.log(body);
         } while (false);
-        return r.toJSON();
+        return r;
+    }
+
+    @Post('download')
+    public download(@Res() res: Response) {
+        const filePath =
+            'D:/work/svn_doc/lr/04-学习库/01-技术规范/01-开发规范/希为科技TypeScript代码规范.docx';
+        res.download(filePath, (err) => {
+            if (!err) {
+                console.log('success', 'download', filePath);
+                return;
+            }
+
+            console.error(err);
+            res.send({ err: -1, msg: String(err) });
+        });
+    }
+
+    @Post('view')
+    public view(@Res() res: Response) {
+        const filePath =
+            'D:/temp/企业微信截图_9df5fd96-30b0-4109-9b98-2fec7880fa07.png';
+        res.sendFile(filePath, (err) => {
+            if (!err) {
+                console.log('success', 'view', filePath);
+                return;
+            }
+            console.error(err);
+            res.send({ err: -1, msg: String(err) });
+        });
     }
 }
