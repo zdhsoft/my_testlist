312b80f4dab965b56aebcd6c407f559222080b6e
zdhsoft <zdhsoft@163.com>
2021-12-21 10:06:01 +0800

    增加yaml使用例子。

--- typescript/04_yaml/yaml/src/main.ts
+++ typescript/04_yaml/yaml/src/main.ts
@@ -1,6 +1,6 @@
 /*************************************************************************
- * Copyright(c) 2017-2021, 
- * 
+ * Copyright(c) 2017-2021,
+ *
  * All rights reserved.
  *
  * @filename main.ts
@@ -11,4 +11,15 @@
  * 1.0                 祝冬华       创建文件            2021-12-21
  *************************************************************************/
 
+import yaml from 'js-yaml';
+import fs from 'fs';
+
+// const fs   = require('fs');
+
 console.log('hello world!');
+const data = fs.readFileSync('ci.yaml', 'utf8');
+const doc = yaml.load(data);
+console.log('------------分隔线---------------');
+console.log(JSON.stringify(doc, null, 2));
+console.log('------------分隔线---------------');
+console.log(yaml.dump(doc));
