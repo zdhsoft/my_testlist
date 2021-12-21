db3ca21304cbceb41c637ed6effa62af467efb19
zdhsoft <zdhsoft@163.com>
2021-12-21 09:34:44 +0800

    增加yaml代码。

--- c++20/firstcpp20/main.cpp
+++ c++20/firstcpp20/main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
 	int a = 9990'9909;
+	int b = 999'999;
+
+	auto m = a <=> b;
+	cout << int(m) << endl;
 
 	cout << "hello world for C++ 20" << endl;
 	cout << a << endl;
