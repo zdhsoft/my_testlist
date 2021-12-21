23399183f8c8beefe52a848b1196ed6c227d26b6
zdhsoft <zdhsoft@163.com>
2021-12-21 14:50:42 +0800

    C#的例子。

---
+++ dotnet/01_ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace Simple
+{
+    class Empty
+    {
+        public override string ToString()
+        {
+            return "hello MyName is Empty";
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is C#");
+            var data = new ArrayList(10)
+            {
+                "Visual Basic",
+                344,
+                55,
+                new Empty()
+            };
+            data.Remove(55);
+
+            foreach (object el in data)
+            {
+                Console.WriteLine(el);
+            }
+        }
+
+}
+}
