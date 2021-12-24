366feb212de6f9f68fca7a812fec82a4478fdfd6
zdhsoft <donghua@shubidatech.com>
2021-12-24 11:05:53 +0800

    修改登录信息。

--- nest/10_auth/src/api/login/login.controller.ts
+++ nest/10_auth/src/api/login/login.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Post, Query, UseGuards, Request } from '@nestjs/common';
 import { UserService } from 'src/api/user/user.service';
 import { LocalGuard } from 'src/common/local.guard';
+
+@UseGuards(LocalGuard)
 @Controller('api/login')
 export class LoginController {
     constructor(private readonly userService: UserService) {
         //
     }
 
-    @UseGuards(LocalGuard)
     @Post('/login')
     async doLogin(@Request() req) {
         return req.user;
