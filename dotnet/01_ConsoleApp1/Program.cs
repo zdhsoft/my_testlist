16a397ecfebebaa45453c783b12b1d395b397cc0
zdhsoft <zdhsoft@163.com>
2021-12-30 09:57:22 +0800

    Update Program.cs

--- dotnet/01_ConsoleApp1/Program.cs
+++ dotnet/01_ConsoleApp1/Program.cs
@@ -3,6 +3,57 @@ using System.Collections;
 
 namespace Simple
 {
+    interface ITest
+    {
+        string GetName();
+        string GetDescription();
+
+        int GetId();
+    }
+
+
+    class XTest : ITest
+    {
+        private int m_id;
+        private string m_name;
+        private string m_description;
+
+        public XTest(int paramId = 0, string paramName = "", string paramDescription = "")
+        {
+            m_id = paramId;
+            m_name = paramName;
+            m_description = paramDescription;
+        }
+
+
+        public string GetDescription()
+        {
+            return m_description;
+        }
+
+        public void SetDescription(string paramDescription)
+        {
+            m_description = paramDescription;
+        }
+
+        public int GetId()
+        {
+            return m_id;
+        }
+
+        public string GetName()
+        {
+            return m_name;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("id:{0}, name:{1}, desc:{2}", m_id, m_name, m_description);
+
+        }
+    }
+
+
     class Empty
     {
         public override string ToString()
@@ -30,6 +81,13 @@ namespace Simple
             {
                 Console.WriteLine(el);
             }
+
+            int m = 100;
+            for (int i = 0; i < m; i++)
+            {
+                Console.WriteLine("%d", i);
+            }
+            
         }
 
 }
