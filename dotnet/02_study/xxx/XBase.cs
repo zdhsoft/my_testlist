0dd34ef835a43fcd8ebcca99853ba64d8c8c2ed8
zdhsoft <zdhsoft@163.com>
2022-01-06 23:21:22 +0800

    学习属性与派生

---
+++ dotnet/02_study/xxx/XBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_study.xxx
+{
+    internal class XBase
+    {
+        private int m_id;
+        private string m_name;
+
+        public XBase(int paramId, string paramName)
+        {
+            m_id = paramId;
+            m_name = paramName;
+        }
+
+        public string Name
+        {
+            get { return m_name; }
+            set { m_name = value; }
+        }
+        public int Id
+        {
+            get { return m_id; }
+            set
+            {
+                if (value > 100)
+                {
+                    m_id = value;
+                }
+                else
+                {
+                    m_id = -value;
+                }
+            }
+        }
+    }
+}
---
+++ dotnet/02_study/xxx/XPerson.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_study.xxx
+{
+    internal class XPerson : XBase
+    {
+        private int m_age;
+        public XPerson() : base(100, "XPerson")
+        {
+            m_age = 0;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return m_age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    m_age = 0;
+                }
+                else if (value > 100)
+                {
+                    m_age = 100;
+                }
+                else
+                {
+                    m_age = value;
+                }
+            }
+        }
+    }
+}
