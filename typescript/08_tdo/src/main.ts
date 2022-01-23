c2d80c6e3cbc38b033f97e9aa4c91469a2698626
zdhsoft <zdhsoft@163.com>
2022-01-23 10:09:39 +0800

    增加数据结构示例。

--- typescript/08_tdo/src/main.ts
+++ typescript/08_tdo/src/main.ts
@@ -1,6 +1,6 @@
 /*************************************************************************
- * Copyright(c) 2017-2022, 
- * 
+ * Copyright(c) 2017-2022,
+ *
  * All rights reserved.
  *
  * @filename main.ts
@@ -11,4 +11,28 @@
  * 1.0                 祝冬华       创建文件            2022-01-22
  *************************************************************************/
 
-console.log('hello world!');
+import { getLogger, Queue, Stack } from 'xmcommon';
+
+const log = getLogger(__filename);
+const stack = new Stack<number>();
+const queue = new Queue<number>();
+
+for (let i = 0; i < 10; i++) {
+    stack.push(i);
+    queue.enqueue(i);
+}
+
+do {
+    const m = stack.pop();
+    if (m === undefined) {
+        break;
+    }
+    log.log('stack:' + m);
+} while (true);
+do {
+    const m = queue.dequeue();
+    if (m === undefined) {
+        break;
+    }
+    log.log('queue:' + m);
+} while (true);
