186d6a4250f2459bdd7b1c9287d7ab94bd9cf662
zdhsoft <donghua@shubidatech.com>
2022-03-19 22:01:31 +0800

    完成pdf文件的读取

--- nest/08_ormdemo/nest笔记五：使用TypeORM连接mysql.md
+++ nest/08_ormdemo/nest笔记五：使用TypeORM连接mysql.md
@@ -183,9 +183,3 @@ export class User {
 
 完后，就可以使用它做基本的数据库操作了。
 
-## 相关文章列表
-- [nest笔记一:创建工程并配置vscode调试环境](https://zdhsoft.blog.csdn.net/article/details/121258677)
-- [nest笔记二:使用log4js替换nest默认日志](https://zdhsoft.blog.csdn.net/article/details/121359315)
-- [nest笔记三:使用全局拦截器打印请求日志](https://zdhsoft.blog.csdn.net/article/details/121393584)
-- [nest笔记四:文件的上传与下载](https://zdhsoft.blog.csdn.net/article/details/121644432)
-- [nest记笔五：使用TypeORM连接mysql](https://zdhsoft.blog.csdn.net/article/details/121644432)
--- typescript/14_readpdf/src/main.ts
+++ typescript/14_readpdf/src/main.ts
@@ -1,14 +1,90 @@
-/*************************************************************************
- * Copyright(c) 2017-2022, 
- * 
- * All rights reserved.
- *
- * @filename main.ts
- * @summary：项目主程序
- * @version: 1.0
- * ------------------------------------------------------------------------
- * version             author            reason             date
- * 1.0                 祝冬华       创建文件            2022-03-19
- *************************************************************************/
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pdfreader = require('pdfreader');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const PDFParser = require('pdf2json');
+const pdfParser = new PDFParser();
+import * as fs from 'fs';
+import _ from 'lodash';
+import { URL } from 'url';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pdfText = require('pdf-text');
+const pdffile = 'd:/生产问题/C107922_19370_20210303T154840_133560105_01.pdf';
 
-console.log('hello world!');
+interface IPageR {
+    T: string;
+    S: number;
+    TS: number[];
+}
+interface IPage {
+    Width: number;
+    Height: number;
+    HLines: { x: number; y: number; w: number; l: number; oc: string }[];
+    VLines: { x: number; y: number; w: number; l: number; oc: string }[];
+    Fills: any[];
+    Texts: {
+        x: number;
+        y: number;
+        w: number;
+        sw: number;
+        A: string;
+        R: IPageR[];
+    }[];
+}
+
+interface IPDFJson {
+    Transcoder: string;
+    Meta: {
+        PDFFormatVersion: string;
+        IsAcroFormPresent: boolean;
+        IsXFAPresent: boolean;
+        Producer: string;
+        CreationDate: string;
+        ModDate: string;
+        Metadata: any;
+    };
+    Pages: IPage[];
+}
+
+function FilterT(pdf: IPDFJson) {
+    const TextList: string[] = [];
+    pdf?.Pages?.forEach((page) => {
+        page?.Texts?.forEach((text) => {
+            text.R.forEach((R) => {
+                //
+                // URL.
+                const T = R?.T;
+                if (T !== undefined && T !== null) {
+                    R.T = decodeURIComponent(R.T);
+                    TextList.push(R.T);
+                }
+                // console.log(decodeURIComponent(R.T));
+            });
+        });
+    });
+
+    fs.writeFileSync('./t.txt', TextList.join('\n'), 'utf8');
+}
+
+pdfParser.on('pdfParser_dataError', (errData: any) =>
+    console.error(errData.parserError),
+);
+pdfParser.on('pdfParser_dataReady', (pdfData: IPDFJson) => {
+    const d = pdfParser.getRawTextContent();
+    fs.writeFileSync('./raw.txt', String(d));
+    FilterT(pdfData);
+    fs.writeFileSync('./F1040EZ.json', JSON.stringify(pdfData, null, 2));
+});
+
+pdfParser.on('readable', (meta: any) =>
+    console.log('PDF Metadata', JSON.stringify(meta, null, 2)),
+);
+pdfParser.on('data', (page: any) =>
+    console.log(
+        page ? 'One page paged' : 'All pages parsed',
+        JSON.stringify(page, null, 2),
+    ),
+);
+pdfParser.on('error', (err: any) => console.error('Parser Error', err));
+
+const data = fs.readFileSync(pdffile);
+pdfParser.parseBuffer(data);
