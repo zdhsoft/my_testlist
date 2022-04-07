9ca50c6f9c51a8537a211a0d33efdf5ec217a36f
zdhsoft <donghua@shubidatech.com>
2022-04-07 17:13:09 +0800

    增加BCB新工程

---
+++ c++builder/05_week_report/form_main.h
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------------------
+
+#ifndef form_mainH
+#define form_mainH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.ComCtrls.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.FileCtrl.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
