681ea0a0a5372ee3777c8a26a72e0b800706ee40
zdhsoft@163.com <zdhsoft>
2022-05-18 17:43:16 +0800

    实现去除所有空白字符的功能。

--- typescript/08_tdo/src/main.ts
+++ typescript/08_tdo/src/main.ts
@@ -1,38 +1,35 @@
-/*************************************************************************
- * Copyright(c) 2017-2022,
- *
- * All rights reserved.
- *
- * @filename main.ts
- * @summary：项目主程序
- * @version: 1.0
- * ------------------------------------------------------------------------
- * version             author            reason             date
- * 1.0                 祝冬华       创建文件            2022-01-22
- *************************************************************************/
-
 import { getLogger, Queue, Stack } from 'xmcommon';
 
 const log = getLogger(__filename);
-const stack = new Stack<number>();
-const queue = new Queue<number>();
+// const stack = new Stack<number>();
+// const queue = new Queue<number>();
 
-for (let i = 0; i < 10; i++) {
-    stack.push(i);
-    queue.enqueue(i);
+// for (let i = 0; i < 10; i++) {
+//     stack.push(i);
+//     queue.enqueue(i);
+// }
+
+// do {
+//     const m = stack.pop();
+//     if (m === undefined) {
+//         break;
+//     }
+//     log.log('stack:' + m);
+// } while (true);
+// do {
+//     const m = queue.dequeue();
+//     if (m === undefined) {
+//         break;
+//     }
+//     log.log('queue:' + m);
+// } while (true);
+
+function trimstring(paramV: string) {
+    // return paramV.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+    return paramV.replace(/[\s\uFEFF\xA0]/g, '');
 }
 
-do {
-    const m = stack.pop();
-    if (m === undefined) {
-        break;
-    }
-    log.log('stack:' + m);
-} while (true);
-do {
-    const m = queue.dequeue();
-    if (m === undefined) {
-        break;
-    }
-    log.log('queue:' + m);
-} while (true);
+const t = ['aaa', ' aaa', 'aaa ', '中!！ 中a a \n\t\r'];
+t.forEach((v) => {
+    log.info(`[${v}] => a:[${trimstring(v)}] , b:[${v.trim()}] c:{${v}}`);
+});
