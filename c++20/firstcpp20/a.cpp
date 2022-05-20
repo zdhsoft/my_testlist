558d5df7576ab46edc88c17906b5502570d99563
zdhsoft@163.com <zdhsoft>
2022-05-20 17:43:40 +0800

    增加部分C++的代码。

--- c++20/firstcpp20/a.cpp
+++ c++20/firstcpp20/a.cpp
@@ -1,7 +1,17 @@
 #include "a.h"
 #include <iostream>
+#include <map>
 using namespace std;
 int test2017() {
 	cout << "test 2017!" << endl;
+	map<int, int> m;
+	m.insert(std::pair(1, 2));
+	m.insert(std::pair(2, 4));
+	m.insert(std::pair(3, 6));
+	m.insert(std::pair(4, 8));
+	for (const auto& [k, v] : m) {
+		cout << "[" << k << "]=" << int(v);
+		cout << endl;
+	}
 	return 0;
 }
