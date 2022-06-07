fdc14c712c20fe15f3b0f4d032bc7383df57ba35
zdhsoft@163.com <zdhsoft>
2022-06-07 11:58:00 +0800

    填写实验性质代码。

--- typescript/18_redis/redis/src/main.ts
+++ typescript/18_redis/redis/src/main.ts
@@ -8,7 +8,50 @@
  * @version: 1.0
  * ------------------------------------------------------------------------
  * version             author            reason             date
- * 1.0                 祝冬华       创建文件            2022-03-28
+ * 1.0                 zdhsoft       创建文件            2022-03-28
  *************************************************************************/
 
-console.log('hello world!');
+import * as redis from 'redis';
+// export interface RedisClientOptions<M extends RedisModules = RedisModules, F extends RedisFunctions = RedisFunctions, S extends RedisScripts = RedisScripts> extends RedisExtensions<M, F, S> {
+//     url?: string;
+//     socket?: RedisSocketOptions;
+//     username?: string;
+//     password?: string;
+//     name?: string;
+//     database?: number;
+//     commandsQueueMaxLength?: number;
+//     disableOfflineQueue?: boolean;
+//     readonly?: boolean;
+//     legacyMode?: boolean;
+//     isolationPoolOptions?: PoolOptions;
+// }
+async function initRedis() {
+    //
+    // import { createClient } from 'redis';
+    const opts = {
+        host: '127.0.0.1',
+        port: 6379,
+        database: 1,
+    };
+    const client = redis.createClient(opts);
+    await client.connect();
+    return client;
+}
+
+async function main() {
+    const client = await initRedis();
+    const s = client.set('aaaaa', 1999, { EX: 2600 });
+    const r = await client.get('aaaaa');
+    s.then(
+        (...a) => {
+            console.log('SUCCESS:' + JSON.stringify(a));
+        },
+        (...b) => {
+            'FAIL:' + JSON.stringify(b);
+        },
+    );
+    // console.log('set result:' + s);
+    console.log('get result:' + JSON.stringify(r, null, 2));
+}
+
+main();
