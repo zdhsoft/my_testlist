3e9785540210badd00740ffd7316269fc01498d7
zdhsoft <zdhsoft@163.com>
2022-07-01 16:58:25 +0800

    实现electron的功能

---
+++ electron/04_utools/main.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    win.loadFile('index.html')
+}
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+console.log('hello world');
