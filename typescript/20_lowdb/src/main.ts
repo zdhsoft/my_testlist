74e9a6a7e23503e4529e94f6fd2bf799631a84e8
zdhsoft <zdhsoft@163.com>
2022-07-08 14:15:43 +0800

    使用lowdb测试

--- typescript/20_lowdb/src/main.ts
+++ typescript/20_lowdb/src/main.ts
@@ -11,4 +11,20 @@
  * 1.0                 zdhsoft       创建文件            2022-03-28
  *************************************************************************/
 
-console.log('hello world!');
+import path from 'path';
+import { LowSync, JSONFileSync } from 'lowdb';
+import url from 'url';
+import { getLogger } from 'xmcommon';
+
+const log = getLogger(__filename);
+
+type Data = { posts: string[] };
+
+const file = path.join(__dirname, 'db.json');
+const adapter = new JSONFileSync<Data>(file);
+const db = new LowSync<Data>(adapter);
+db.read();
+db.data ||= { posts: [] };
+db.data.posts.push('hello');
+db.write();
+log.log('hello world!');
