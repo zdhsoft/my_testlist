7de8f999aa0b864af458df9bb69ddce4b08e63e6
zdhsoft <zdhsoft@163.com>
2022-07-08 14:16:58 +0800

    去除lowdb

--- typescript/20_lowdb/src/main.ts
+++ typescript/20_lowdb/src/main.ts
@@ -10,21 +10,7 @@
  * version             author            reason             date
  * 1.0                 zdhsoft       创建文件            2022-03-28
  *************************************************************************/
-
-import path from 'path';
-import { LowSync, JSONFileSync } from 'lowdb';
-import url from 'url';
 import { getLogger } from 'xmcommon';
 
 const log = getLogger(__filename);
-
-type Data = { posts: string[] };
-
-const file = path.join(__dirname, 'db.json');
-const adapter = new JSONFileSync<Data>(file);
-const db = new LowSync<Data>(adapter);
-db.read();
-db.data ||= { posts: [] };
-db.data.posts.push('hello');
-db.write();
 log.log('hello world!');
