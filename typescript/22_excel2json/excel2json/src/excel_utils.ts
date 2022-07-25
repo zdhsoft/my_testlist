e0992b04c0c98e502ab78916fa0de1498e360c1e
zdhsoft <zdhsoft@163.com>
2022-07-25 16:29:13 +0800

    初始化工程

---
+++ typescript/22_excel2json/excel2json/src/excel_utils.ts
@@ -0,0 +1,27 @@
+import * as xlsx from 'xlsx';
+export class XExcelUtils {
+    /**
+     * 读取excel 信息并返回：
+     * @param paramFilePath 读取的文件路径
+     */
+    public static readExcelByFile(paramFilePath: string) {
+        const book = xlsx.readFile(paramFilePath); //  xlsx.readFileSync(filePath);
+        // 取第一个Sheet页面
+        const sheet = book.Sheets[book.SheetNames[0]];
+        // 默认第一行是key，key不存在时，值也不显示
+        const sheetObject = xlsx.utils.sheet_to_json(sheet);
+        return sheetObject;
+    }
+    /**
+     * 读取excel 信息并返回：
+     * @param paramData 读取的文件路径
+     */
+    public static readExcelByBuffer(paramData: Buffer) {
+        const book = xlsx.read(paramData, { type: 'buffer' });
+        // 取第一个Sheet页面
+        const sheet = book.Sheets[book.SheetNames[0]];
+        // 默认第一行是key，key不存在时，值也不显示
+        const sheetObject = xlsx.utils.sheet_to_json(sheet);
+        return sheetObject;
+    }
+}
