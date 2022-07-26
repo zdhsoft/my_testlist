2e0e0200e2ad58f3a64d2e660214c539a8d4f4e8
zdhsoft <zdhsoft@163.com>
2022-07-26 10:54:11 +0800

    完成数据初始化。

--- typescript/22_excel2json/excel2json/src/constant.ts
+++ typescript/22_excel2json/excel2json/src/constant.ts
@@ -3,6 +3,15 @@ export enum EnumOutType {
     SERVER = 's',
 }
 
+export enum EnumDataBaseType {
+    UNKNOWN = 0,
+    BOOL = 1,
+    INT = 2,
+    NUMBER = 3,
+    STRING = 4,
+    OBJECT = 5,
+    ANY = 6,
+}
 export enum EnumDataType {
     ANY = 'any',
     BOOL = 'bool',
@@ -46,22 +55,45 @@ export class XTypeUtils {
     public static isAny(paramType: EnumDataType) {
         return TypeSet.SetTypeAny.has(paramType);
     }
+
     public static isBool(paramType: EnumDataType) {
         return TypeSet.SetTypeBool.has(paramType);
     }
+
     public static isInt(paramType: EnumDataType) {
         return TypeSet.SetTypeInt.has(paramType);
     }
+
     public static isNumber(paramType: EnumDataType) {
         return TypeSet.SetTypeNumber.has(paramType);
     }
+
     public static isString(paramType: EnumDataType) {
         return TypeSet.SetTypeString.has(paramType);
     }
+
     public static isObject(paramType: EnumDataType) {
         return TypeSet.SetTypeObject.has(paramType);
     }
+
     public static isArray(paramType: EnumDataType) {
         return TypeSet.SetTypeArray.has(paramType);
+    }
+
+    public static baseType(paramType: EnumDataType): EnumDataBaseType {
+        if (this.isAny(paramType)) {
+            return EnumDataBaseType.ANY;
+        } else if (this.isBool(paramType)) {
+            return EnumDataBaseType.BOOL;
+        } else if (this.isInt(paramType)) {
+            return EnumDataBaseType.INT;
+        } else if (this.isString(paramType)) {
+            return EnumDataBaseType.STRING;
+        } else if (this.isNumber(paramType)) {
+            return EnumDataBaseType.NUMBER;
+        } else if (this.isObject(paramType)) {
+            return EnumDataBaseType.OBJECT;
+        }
+        return EnumDataBaseType.UNKNOWN;
     }
 }
--- typescript/22_excel2json/excel2json/src/main.ts
+++ typescript/22_excel2json/excel2json/src/main.ts
@@ -18,7 +18,7 @@ import { XExcelUtils } from './excel_utils';
 const xlsxfile = './testdata/Build.xlsx';
 const t = './testdata/Build.json';
 import { getLogger, XCommonRet, utils } from 'xmcommon';
-import { EnumOutType } from './constant';
+import { EnumDataBaseType, EnumDataType, EnumOutType, XTypeUtils } from './constant';
 const log = getLogger(__filename);
 
 interface ITableInfo {
@@ -31,20 +31,111 @@ interface ITableInfo {
 const tablelist = 'tablelist';
 
 class XColumnInfo {
-    public constructor() {
-        //
+    public name: string;
+    public key: string;
+    public type: EnumDataBaseType;
+    public isArray: boolean;
+    public outType: string;
+    public constructor(paramKey: string, paramName: string) {
+        this.name = paramName;
+        this.key = paramKey;
+        this.type = EnumDataBaseType.UNKNOWN;
+        this.isArray = false;
+        this.outType = '';
+    }
+    public toJSON() {
+        return {
+            name: this.name,
+            key: this.key,
+            type: this.type,
+            isArray: this.isArray,
+            outType: this.outType,
+        };
     }
 }
 
 class XTitleInfo {
     public columns: XColumnInfo[] = [];
-    public columnMap: Map<string, XColumnInfo> = new Map();
+    public columnKeyMap = new Map<string, XColumnInfo>();
+    public columnNameMap = new Map<string, XColumnInfo>();
     public constructor() {
         //
     }
     public reset() {
         this.columns = [];
-        this.columnMap.clear();
+        this.columnKeyMap.clear();
+        this.columnNameMap.clear();
+    }
+
+    public toJSON() {
+        return {
+            columns: this.columns,
+        };
+    }
+
+    public getColumnByKey(paramKey: string): XCommonRet<XColumnInfo> {
+        const r = new XCommonRet<XColumnInfo>();
+        do {
+            if (!utils.isString(paramKey)) {
+                r.setError(51, `输入的key:${paramKey}不是字符串`);
+                break;
+            }
+            const k = paramKey.trim();
+            if (utils.isEmpty(k)) {
+                r.setError(52, `输入的key是空串!`);
+                break;
+            }
+            const col = this.columnKeyMap.get(k) as XColumnInfo;
+            if (utils.isNull(col)) {
+                r.setError(53, `未找到key:${k}的列定义`);
+                break;
+            }
+            r.setOK(col);
+
+            this.columns.push(col);
+        } while (false);
+        return r;
+    }
+
+    public addColumn(paramKey: string, paramName: string): XCommonRet<XColumnInfo> {
+        const r = new XCommonRet<XColumnInfo>();
+        do {
+            if (!utils.isString(paramKey)) {
+                r.setError(41, `输入的key:${paramKey}不是字符串`);
+                break;
+            }
+            if (!utils.isString(paramName)) {
+                r.setError(42, `输入的Name:${paramName}不是字符串`);
+                break;
+            }
+            const k = paramKey.trim();
+            const n = paramName.trim();
+            if (utils.isEmpty(k)) {
+                r.setError(43, `输入的key是空串!`);
+                break;
+            }
+
+            if (utils.isEmpty(n)) {
+                r.setError(44, `输入的name是空串!`);
+                break;
+            }
+
+            if (this.columnKeyMap.has(k)) {
+                r.setError(45, `输入的key:${k}是重复的!`);
+                break;
+            }
+
+            if (this.columnNameMap.has(n)) {
+                r.setError(46, `输入的name:${n}是重复的!`);
+                break;
+            }
+
+            const columnInfo = new XColumnInfo(k, n);
+            this.columnKeyMap.set(k, columnInfo);
+            this.columnNameMap.set(n, columnInfo);
+            r.setOK(columnInfo);
+        } while (false);
+        return r;
     }
 }
 
@@ -55,6 +146,7 @@ class XTableInfo {
     public describe: string;
 
     public titleInfo: XTitleInfo;
+    public data: any[];
     public constructor(paramTypes: string, paramSheetName: string, paramFileName: string, paramDescribe = '') {
         this.types = [];
         this.sheetName = paramSheetName;
@@ -62,6 +154,7 @@ class XTableInfo {
         this.describe = paramDescribe;
         this.setTypes(paramTypes);
         this.titleInfo = new XTitleInfo();
+        this.data = [];
     }
 
     public setTypes(paramTypes: string) {
@@ -73,9 +166,21 @@ class XTableInfo {
         }
     }
 
+    public toJSON() {
+        return {
+            sheetName: this.sheetName,
+            describe: this.describe,
+            types: this.types,
+            fileName: this.fileName,
+            title: this.titleInfo,
+            data: this.data,
+        };
+    }
+
     public init(paramSheetData: any[]): XCommonRet {
         const r = new XCommonRet();
         do {
+            this.titleInfo.reset();
             //
             if (!Array.isArray(paramSheetData)) {
                 //
@@ -83,9 +188,106 @@ class XTableInfo {
                 break;
             }
 
-            if (paramSheetData.length < 2) {
-                r.setError(-22, `初始化表的表头小于2行!`);
+            if (paramSheetData.length < 3) {
+                r.setError(-22, `初始化表的表头小于3行!`);
                 break;
+            }
+
+            const keyName = paramSheetData[0];
+            if (utils.isNull(keyName)) {
+                r.setError(-23, `定义Key和Name的第一行为:${keyName}`);
+                break;
+            }
+            if (!utils.isObject(keyName)) {
+                r.setError(-24, `定义Key和Name的第一行为:${keyName}, 不是object类型!`);
+                break;
+            }
+            // 初始化name,key
+            for (const key in keyName) {
+                const name = keyName[key];
+                const addResult = this.titleInfo.addColumn(key, name);
+                if (addResult.isNotOK) {
+                    r.assignFrom(addResult);
+                    break;
+                }
+            }
+            if (r.isNotOK) {
+                break;
+            }
+            // 初始化类型
+            const colType = paramSheetData[1];
+            if (utils.isNull(colType)) {
+                r.setError(-23, `定义类型的第二行为:${colType}`);
+                break;
+            }
+            if (!utils.isObject(keyName)) {
+                r.setError(-24, `定义类型的第二行为:${colType}, 不是object类型!`);
+                break;
+            }
+            for (const key in colType) {
+                let t: string = colType[key];
+                if (utils.isNull(t)) {
+                    r.setError(-26, `定义的类型值为:${t}, 不是有效的类型!`);
+                    break;
+                }
+                if (!utils.isString(t)) {
+                    r.setError(-25, `定义类型值值为:${t}, 不是字符串类型`);
+                    break;
+                }
+                t = t.trim();
+                const type = XTypeUtils.baseType(t as EnumDataType);
+                if (type === EnumDataBaseType.UNKNOWN) {
+                    r.setError(-27, `定义的类型值为:${t}, 不是预设的类型!`);
+                    break;
+                }
+
+                const isArray = XTypeUtils.isArray(t as EnumDataType);
+                const getResult = this.titleInfo.getColumnByKey(key);
+                if (getResult.isNotOK) {
+                    r.assignFrom(getResult);
+                    r.addErrorPre(`实始化类型:Key=${key}, type:${t}`);
+                    break;
+                }
+
+                const column = getResult.data as XColumnInfo;
+                column.isArray = isArray;
+                column.type = type;
+            }
+            // 初始化输出类型
+            const outType = paramSheetData[2];
+            if (utils.isNull(outType)) {
+                r.setError(-28, `定义类型的第三行为:${outType}`);
+                break;
+            }
+            if (!utils.isObject(keyName)) {
+                r.setError(-29, `定义类型的第三行为:${outType}, 不是object类型!`);
+                break;
+            }
+            for (const key in outType) {
+                let t: string = outType[key];
+                if (utils.isNull(t)) {
+                    r.setError(-30, `定义的输出类型值为:${t}, 不是有效的类型!`);
+                    break;
+                }
+                if (!utils.isString(t)) {
+                    r.setError(-31, `定义类输出类型值为:${t}, 不是字符串类型`);
+                    break;
+                }
+                t = t.trim();
+                const getResult = this.titleInfo.getColumnByKey(key);
+                if (getResult.isNotOK) {
+                    r.assignFrom(getResult);
+                    r.addErrorPre(`实始化输出类型:Key=${key}, 输出类型:${t}`);
+                    break;
+                }
+
+                const column = getResult.data as XColumnInfo;
+                column.outType = t;
+            }
+            // 假加数据
+            const dataLength = paramSheetData.length;
+            for (let rowIndex = 3; rowIndex < dataLength; rowIndex++) {
+                this.data.push(paramSheetData[rowIndex]);
             }
         } while (false);
         return r;
@@ -116,7 +318,6 @@ function initTable(paramTableInfoList: ITableInfo[], paramExcelData: any): XComm
         for (const info of paramTableInfoList) {
             log.info(`处理:${info.sheetName}:${info.describe}`);
             const s = new XTableInfo(info.type, info.sheetName, info.filename, info.describe);
-            log.info(`  ==>${JSON.stringify(s)}`);
 
             const sheet = paramExcelData[info.sheetName];
             if (utils.isNull(sheet)) {
@@ -128,6 +329,8 @@ function initTable(paramTableInfoList: ITableInfo[], paramExcelData: any): XComm
             if (initResult.isNotOK) {
                 r.assignFrom(initResult);
                 r.addErrorPre(`初始化:${info.sheetName}:${info.describe}`);
+            } else {
+                // log.info('----->\n' + JSON.stringify(s, null, 2));
             }
             tableInfo.push(s);
         }
