4cc33e13bbbdcc305d56e9c8d495256d7409a4bf
zdhsoft <zdhsoft@163.com>
2022-10-09 12:24:35 +0800

    编写Handlebars示例

--- typescript/24_handlebars/src/main.ts
+++ typescript/24_handlebars/src/main.ts
@@ -10,5 +10,8 @@
  * version             author            reason             date
  * 1.0                 zdhsoft      创建文件            2022-10-09
  *************************************************************************/
+import Handlebars from 'handlebars';
 
 console.log('hello world!');
+const template = Handlebars.compile('Name: {{name}} >>>> age: {{age}}');
+console.log(template({ name: 'Nils', age: 18 }));
