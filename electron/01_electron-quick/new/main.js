1a9be21549c41e57ffce1794df84c33bf8beb1bb
zdhsoft <zdhsoft@163.com>
2022-10-20 00:00:38 +0800

    注销报错代码。

--- electron/01_electron-quick/new/main.js
+++ electron/01_electron-quick/new/main.js
@@ -41,7 +41,19 @@ const createWindow = () => {
     app.on('before-quit', () => {
         clearInterval(progressInterval)
     });
-
+    // win.setThumbarButtons([
+    //   {
+    //     tooltip: 'button1',
+    //     icon: path.join(__dirname, 'button1.png'),
+    //     click () { console.log('button1 clicked') }
+    //   }, {
+    //     tooltip: 'button2',
+    //     icon: path.join(__dirname, 'button2.png'),
+    //     flags: ['enabled', 'dismissonclick'],
+    //     click () { console.log('button2 clicked.') }
+    //   }
+    // ]);
+    // win.setOverlayIcon(path.join('.','icon.png'), 'Description for overlay')
 }
 let tray
 
