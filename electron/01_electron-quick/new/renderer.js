929486a949835fa5344d8420df2c81a2d98b8922
zdhsoft <zdhsoft@163.com>
2022-10-19 23:36:58 +0800

    增加通知示例

--- electron/01_electron-quick/new/renderer.js
+++ electron/01_electron-quick/new/renderer.js
@@ -7,3 +7,8 @@ document.getElementById('reset-to-system').addEventListener('click', async () =>
     await window.darkMode.system();
     document.getElementById('theme-source').innerHTML = 'System';
 })
+
+const t = 'Title';
+const b = 'Nofification from the renderer process. click to log to console.';
+const m = 'notification clicked!';
+new Notification(t, {body: b}).click = () => document.getElementById('output').innerText = m;
