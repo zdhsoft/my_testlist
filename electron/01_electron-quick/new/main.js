7bc13aabeb283f0b4f6a1f883e90be5e45b106d8
zdhsoft <zdhsoft@163.com>
2022-10-19 23:42:27 +0800

    增加dock

--- electron/01_electron-quick/new/main.js
+++ electron/01_electron-quick/new/main.js
@@ -47,6 +47,19 @@ app.whenReady().then(() => {
     createWindow()
 });
 
+const dockMenu = Menu.buildFromTemplate([
+  {
+    label: 'New Window',
+    click () { console.log('New Window') }
+  }, {
+    label: 'New Window with Settings',
+    submenu: [
+      { label: 'Basic' },
+      { label: 'Pro' }
+    ]
+  },
+  { label: 'New Command...' }
+])
 
 
 
