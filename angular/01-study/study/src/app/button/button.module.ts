f2d6fe277728687f015b941dbb6656b4acf0e4f8
zdhsoft <zdhsoft@163.com>
2022-10-27 23:39:13 +0800

    补充图标

--- angular/01-study/study/src/app/button/button.module.ts
+++ angular/01-study/study/src/app/button/button.module.ts
@@ -4,9 +4,25 @@ import { ButtonComponent } from './button.component';
 import {NzButtonModule} from "ng-zorro-antd/button";
 import {FormsModule} from "@angular/forms";
 import {NzRadioModule} from "ng-zorro-antd/radio";
+import {
+  StepBackwardOutline,
+  CaretLeftOutline,
+  SettingOutline,
+  DownloadOutline,
+  UserOutline,
+  LockOutline,
+} from '@ant-design/icons-angular/icons';
+import {NzIconModule} from "ng-zorro-antd/icon";
+import {IconDefinition} from "@ant-design/icons-angular";
 
-
-
+const icons: IconDefinition[] = [
+  StepBackwardOutline,
+  CaretLeftOutline,
+  SettingOutline,
+  UserOutline,
+  LockOutline,
+  DownloadOutline
+];
 @NgModule({
   declarations: [
     ButtonComponent
@@ -18,6 +34,7 @@ import {NzRadioModule} from "ng-zorro-antd/radio";
     CommonModule,
     NzButtonModule,
     FormsModule,
+    NzIconModule.forChild(icons),
     NzRadioModule
   ]
 })
