671ad2323fefee26852af1c510d6803206ba8581
zdhsoft <zdhsoft@163.com>
2022-10-27 15:25:52 +0800

    增加控制测试

--- angular/01-study/study/src/app/app.module.ts
+++ angular/01-study/study/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule  } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    MatSliderModule,
     BrowserAnimationsModule
   ],
   providers: [],
