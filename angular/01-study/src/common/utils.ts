511d280e86d5aaf39ae879e8e3b088eca0481499
zdhsoft <zdhsoft@163.com>
2022-10-27 14:24:41 +0800

    删除旧项目

--- angular/01-study/src/common/utils.ts
+++
@@ -1,9 +0,0 @@
-export class XUtils {
-  public static isNull(paramValue: unknown | null | undefined) {
-    return paramValue === undefined || paramValue === null;
-  }
-
-  public static isNotNull(paramValue: undefined | null | unknown) {
-    return !this.isNull(paramValue);
-  }
-}
