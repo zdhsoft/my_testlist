f19c8d9f2fb7c949ee0d2d2092b788831e47d348
zdhsoft <zdhsoft@163.com>
2022-10-29 22:44:59 +0800

    实现todo

--- angular/03_full/todo/src/app/app.component.ts
+++ angular/03_full/todo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoList } from "./todoList";
+import { TodoItem } from "./todoItem";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  private list = new TodoList("Bob", [
+    new TodoItem("Go for run", true),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets"),
+  ]);
+  get username(): string {
+    return this.list.user;
+  }
+  get itemCount(): number {
+    return this.list.items
+      .filter(item => !item.complete).length;
+  }
 }
