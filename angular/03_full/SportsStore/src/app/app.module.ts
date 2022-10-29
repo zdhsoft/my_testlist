14d485dff57dea2e5b283c5cf6b57038d02da936
zdhsoft <zdhsoft@163.com>
2022-10-29 23:26:44 +0800

    初始目录结构

--- angular/03_full/SportsStore/src/app/app.module.ts
+++ angular/03_full/SportsStore/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ModelComponent } from './model/model.component';
+import { AdminComponent } from './admin/admin.component';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelComponent,
+    AdminComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule
