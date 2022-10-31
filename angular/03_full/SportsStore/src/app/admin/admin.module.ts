0f88083a104ecf1936dcbb8f865f2c48dd2e3f01
zdhsoft <zdhsoft@163.com>
2022-10-31 09:34:34 +0800

    完成store的调整

--- angular/03_full/SportsStore/src/app/admin/admin.module.ts
+++ angular/03_full/SportsStore/src/app/admin/admin.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { AdminComponent } from './admin.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [AdminComponent]
 })
 export class AdminModule { }
--- angular/03_full/SportsStore/src/app/app.module.ts
+++ angular/03_full/SportsStore/src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ModelComponent } from './model/model.component';
-import { AdminComponent } from './admin/admin.component';
-import { StoreComponent } from './store/store.component';
+import { StoreModule } from './store/store.module';
+import { ModelModule } from './model/model.module';
+import { AdminModule } from './admin/admin.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModelComponent,
-    AdminComponent,
-    StoreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, StoreModule, ModelModule, AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
