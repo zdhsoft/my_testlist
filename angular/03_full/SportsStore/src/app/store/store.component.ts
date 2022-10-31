fb0f038a18ee02aae97d3da3a10c09c2413bff13
zdhsoft <zdhsoft@163.com>
2022-10-31 23:51:53 +0800

    完成商品中定义。

--- angular/03_full/SportsStore/src/app/store/store.component.ts
+++ angular/03_full/SportsStore/src/app/store/store.component.ts
@@ -7,16 +7,38 @@ import { ProductRepository } from "../model/product.repository";
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
+  public selectedCategory: any = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
 
   constructor(private repository: ProductRepository) { }
 
   ngOnInit(): void {
   }
   get products(): Product[] {
-    return this.repository.getProducts();
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
   get categories(): string[] {
     return this.repository.getCategories();
+  }
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: EventTarget | null) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageCount(): number {
+    return Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage)
   }
 
 }
