81c0ae574597d15230123e3ed6d8f224a376c705
zdhsoft <zdhsoft@163.com>
2022-11-01 23:21:39 +0800

    修复checkout不显示的问题。

--- angular/03_full/SportsStore/src/app/store/checkout.component.ts
+++ angular/03_full/SportsStore/src/app/store/checkout.component.ts
@@ -1,15 +1,19 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { Order } from "../model/order.model";
 import { OrderRepository } from "../model/order.repository";
+import { Order } from "../model/order.model";
+
 @Component({
-    template: "checkout.component.html"
+    templateUrl: "checkout.component.html",
+    styleUrls: ["checkout.component.css"]
 })
 export class CheckoutComponent {
-    public orderSent: boolean = false;
-    public submitted: boolean = false;
+    orderSent: boolean = false;
+    submitted: boolean = false;
+
     constructor(public repository: OrderRepository,
         public order: Order) { }
+
     submitOrder(form: NgForm) {
         this.submitted = true;
         if (form.valid) {
