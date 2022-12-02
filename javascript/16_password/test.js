298daf298fb182e55f9ba42d253b314d6a603997
zdhsoft@163.com <zdhsoft@163.com>
2022-12-02 21:45:33 +0800

    增加正则测试

--- javascript/16_password/test.js
+++ javascript/16_password/test.js
@@ -1,8 +1,12 @@
 function main() {
-    const reg = /^\w+$/
-    const t = ['aaa', 'aa123', 'Abcdefg123', '_', '___', ' ', '','*&&^'];
+    const reg = /^-?([1-9]\d*|[1-9]\d*\.\d*|0\.\d*[1-9]\d*|0?\.0+|0)$/;
+    const r2 = /^(\-|\+)?\d+(\.\d+)?$/;
+
+
+    // const reg = /^\w+$/
+    const t = ['79690', '79690.1', '-12345', '135.345', '.1345', '-0.1345', '0.123456','*&&^'];
     for(const tv of t) {
-        console.log(`value:[${tv}] result:${reg.test(tv)}`);
+        console.log(`value:[${tv}] result:${reg.test(tv)}, :${r2.test(tv)} => ${+tv}`);
     }
 }
 main();
