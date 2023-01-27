675aac5a5dc96a20c95d8a9c02e598596dfeff96
zdhsoft@163.com <zdhsoft@163.com>
2023-01-27 22:07:33 +0800

    finish first sample

---
+++ delphi/01_study/Unit1.dfm
@@ -0,0 +1,55 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object Edit1: TEdit
+    Left = 32
+    Top = 24
+    Width = 105
+    Height = 23
+    TabOrder = 0
+    Text = 'Edit1'
+  end
+  object Edit2: TEdit
+    Left = 32
+    Top = 56
+    Width = 105
+    Height = 23
+    TabOrder = 1
+    Text = 'Edit2'
+  end
+  object Edit3: TEdit
+    Left = 32
+    Top = 88
+    Width = 105
+    Height = 23
+    TabOrder = 2
+    Text = 'Edit3'
+  end
+  object ListBox1: TListBox
+    Left = 32
+    Top = 128
+    Width = 169
+    Height = 201
+    ItemHeight = 15
+    TabOrder = 3
+  end
+  object Button1: TButton
+    Left = 424
+    Top = 48
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 4
+    OnClick = Button1Click
+  end
+end
---
+++ delphi/01_study/Unit1.pas
@@ -0,0 +1,50 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    ListBox1: TListBox;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+const
+  UserId: Integer = 1;
+  Password: String = '123456';
+  UserName: String = 'hello test';
+  Num: array[0..9] of char = ('0','1','2','3','4','5','6','7','8','9');
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  i: Integer;
+begin
+  Edit1.Text := IntToStr(UserId);
+  Edit2.Text := Password;
+  Edit3.Text := UserName;
+  for i := 0 to high(Num) do
+    begin
+        // ListBox1.Items.Add('0');
+        ListBox1.Items.Add(Num[i]);
+    end;
+
+end;
+
+end.
