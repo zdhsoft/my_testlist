2ec6e1ebe8da20437c328e1af45bd32cf15f4e84
zdhsoft <zdhsoft@163.com>
2023-01-30 13:48:17 +0800

    完成bug修复

--- typescript/25_wordreplace/wordreplace/src/main.ts
+++ typescript/25_wordreplace/wordreplace/src/main.ts
@@ -32,14 +32,18 @@ interface ISheetObject {
 }
 
 function doReplaceWord(paramRep: ISheetObject) {
-    console.log(`开始替换: ${paramRep.template} => ${paramRep.outname}`);
-    if (fs.existsSync(paramRep.template)) {
-        console.error('模板文件不存在:' + paramRep.template);
+    console.log(`  开始替换: ${paramRep.template} => ${paramRep.outname}`);
+    if (!fs.existsSync(paramRep.template)) {
+        console.error('  模板文件不存在:' + paramRep.template);
         return;
     }
-    //
-    //
-    const content = fs.readFileSync('./template/a.docx', 'binary');
+    const dirname = path.dirname(paramRep.outname);
+    if (!fs.existsSync(dirname)) {
+        console.error('  输出目录不存在, 请确定该目录已经存在：' + dirname);
+        return;
+    }
+
+    const content = fs.readFileSync(paramRep.template, 'binary');
     const zip = new PizZip(content);
     const doc = new Docxtemplater(zip, {
         paragraphLoop: true,
@@ -48,7 +52,7 @@ function doReplaceWord(paramRep: ISheetObject) {
     const m = paramRep.data;
     doc.render(m);
     const buf = doc.getZip().generate({ type: 'nodebuffer', compression: 'DEFLATE' });
-    console.log('保存文件:' + paramRep.outname);
+    console.log('  保存文件:' + paramRep.outname);
     fs.writeFileSync(paramRep.outname, buf);
 }
 
@@ -66,12 +70,13 @@ function doReplaceSheet(paramSheetName: string, paramSheet: IReplaceKey[]) {
         o.list.push(d);
     }
     for (const [r, v] of m) {
-        console.log('    ' + JSON.stringify(r) + '\n' + JSON.stringify(v, null, 2));
+        // console.log('    ' + JSON.stringify(r) + '\n' + JSON.stringify(v, null, 2));
         doReplaceWord(v);
     }
 }
 
 function main() {
+    console.log('word关键字替换工具 1.0');
     const r = XExcelUtils.readExcelByFile(path.join('.', filename));
     if (r.isNotOK) {
         console.log(r.getErrorInfo());
@@ -80,12 +85,13 @@ function main() {
 
     const sheets = r.data as object;
 
-    console.log(JSON.stringify(r.data, null, 2));
-    console.log('-----------------------------------------------------');
+    // console.log(JSON.stringify(r.data, null, 2));
+    // console.log('-----------------------------------------------------');
     for (const s in sheets) {
         doReplaceSheet(s, sheets[s]);
     }
 
+    console.log('完成所有替换,请检查是否符合要求!');
     //
     // const content = fs.readFileSync('./template/a.docx', 'binary');
     // const zip = new PizZip(content);
