58c9c0e898fbeb7dcca09d1a90db818222de9ca0
zdhsoft <zdhsoft@163.com>
2023-04-07 17:57:13 +0800

    增加codeblocks代码。

---
+++ codeblocks/hello/hello/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    for(int i = 0; i < 100; i++) {
+        cout << i << endl;
+    }
+    return 0;
+}
