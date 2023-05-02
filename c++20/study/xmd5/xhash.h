3c0794d8c012c68ef2a4ad8806eea54a02d36634
zdhsoft <zdhsoft@163.com>
2023-05-02 17:41:31 +0800

    定义hash头文件

--- c++20/study/xmd5/xhash.h
+++ c++20/study/xmd5/xhash.h
@@ -1,12 +1,15 @@
 #pragma once
 #ifndef _X_HASH_H_
 #define _X_HASH_H_
-
+#include <cstddef>
+#include <vector>
+#include <string>
+using namespace std;
 
 class IHash {
 	virtual const char* hashName() = 0;
 	virtual void update(const void* paramData, const int paramDataBytes) = 0;
-	virtual void digest(const char[16] paramData) = 0;
-
+	virtual void digest(vector<unsigned char> & paramDigestData) = 0;
+	virtual void digest(string& paramDigestString) = 0;
 };
 #endif
