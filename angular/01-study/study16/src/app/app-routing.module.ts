015233856fd4f55d81ba9ddf19722d200de34b88
zdhsoft <zdhsoft@163.com>
2023-05-15 17:57:27 +0800

    增加dashboard

--- angular/01-study/study16/src/app/app-routing.module.ts
+++ angular/01-study/study16/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import {  CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-    { path: 'heroes', component: HeroesComponent }
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
--- angular/01-study/study16/src/app/app.module.ts
+++ angular/01-study/study16/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessageComponent } from './message/message.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessageComponent
+    MessageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
