ac1c96a4e9f2958d14fd5f6d30f3fb558db4bdac
zdhsoft <zdhsoft@163.com>
2023-05-15 17:53:06 +0800

    增加路由

--- angular/01-study/study16/src/app/app-routing.module.ts
+++ angular/01-study/study16/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import {  CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
