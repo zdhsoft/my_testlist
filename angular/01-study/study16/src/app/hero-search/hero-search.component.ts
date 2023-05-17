cc9f63a16b007d191797700723f7960d11c5eb32
zdhsoft <zdhsoft@163.com>
2023-05-17 13:17:27 +0800

    编写rx示例

--- angular/01-study/study16/src/app/hero-search/hero-search.component.ts
+++ angular/01-study/study16/src/app/hero-search/hero-search.component.ts
@@ -26,16 +26,9 @@ export class HeroSearchComponent implements OnInit {
 
     ngOnInit(): void {
         this.heroes$ = this.searchTerms.pipe(
-            // wait 300ms after each keystroke before considering the term
             debounceTime(300),
-
-            // ignore new term if same as previous term
             distinctUntilChanged(),
-
-            // switch to new search observable each time the term changes
-            //switchMap((term: string) => this.heroService.searchHeroesArray(term)),
-        ).subscribe((term) => {
-            return this.heroService.searchHeroesArray(term);
-        });
+            switchMap((term: string) => this.heroService.searchHeroesArray(term)),
+        );
     }
 }
--- typescript/27_rx/src/main.ts
+++ typescript/27_rx/src/main.ts
@@ -10,5 +10,47 @@
  * version             author            reason             date
  * 1.0                 zdhsoft      创建文件            2023-05-17
  *************************************************************************/
+import { Observable, debounceTime, distinctUntilChanged, of, switchMap, Subject } from 'rxjs';
+
+const observable = of(1, 2, 3);
+
+const s = new Subject<number>();
+
+function touch(paramV: number) {
+    console.log('touch...' + paramV);
+    s.next(paramV);
+}
+const k = s.pipe(
+    debounceTime(300),
+    distinctUntilChanged(),
+    switchMap((term) => {
+        console.log('aaaa:' + term);
+        return of(term * 10);
+    }),
+);
+let i = 0;
+let n = 0;
+setInterval(() => {
+    const dt = Date.now();
+    if (dt > n) {
+        touch(i);
+        i++;
+        n = dt + ((i % 5) + 1) * 100;
+    }
+}, 200);
+
+setTimeout(() => {
+    k.subscribe((_k) => {
+        console.log('jjjjj', _k);
+    });
+}, 4000);
+
+observable.subscribe((message) => {
+    console.log('xxxx', message);
+});
+
+// k.subscribe((_k) => {
+//     console.log('kkkk:', _k);
+// });
 
 console.log('hello world!');
