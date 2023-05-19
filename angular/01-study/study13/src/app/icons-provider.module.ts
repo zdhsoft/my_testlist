309cc3bc2bf62a97f10c37c7b8e44aa15615ed38
zdhsoft <zdhsoft@163.com>
2023-05-19 17:07:45 +0800

    增加13的工程

--- angular/01-study/study13/src/app/icons-provider.module.ts
+++ angular/01-study/study13/src/app/icons-provider.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { NZ_ICONS, NzIconModule } from 'ng-zorro-antd/icon';
+// import * as AllIcons from '@ant-design/icons-angular/icons'
+import { IconDefinition } from '@ant-design/icons-angular';
+
 
 import {
   MenuFoldOutline,
   MenuUnfoldOutline,
   FormOutline,
-  DashboardOutline
+  DashboardOutline,
+  LockOutline,
+  CopyOutline,
 } from '@ant-design/icons-angular/icons';
 
-const icons = [MenuFoldOutline, MenuUnfoldOutline, DashboardOutline, FormOutline];
+// const antDesignIcons = AllIcons as {
+//   [key: string]: IconDefinition;
+// };
+// const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
+
+const icons = [MenuFoldOutline, MenuUnfoldOutline, DashboardOutline, FormOutline, LockOutline, CopyOutline];
 
 @NgModule({
-  imports: [NzIconModule],
+  imports: [NzIconModule.forRoot(icons)],
   exports: [NzIconModule],
   providers: [
     { provide: NZ_ICONS, useValue: icons }
---
+++ c++20/study/xmd5/simple.ts
@@ -0,0 +1,12 @@
+export enum s {
+    A = 1,
+    B = 2,
+}
+export class XSimple {
+
+}
+
+export interface IRULE {
+    name: string;
+    age: number;
+}
