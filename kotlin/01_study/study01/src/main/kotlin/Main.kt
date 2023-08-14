00bb1149343760569262969539ba0232c073c133
zdhsoft <zdhsoft@163.com>
2023-08-14 16:26:55 +0800

    补充笔记和新知识

--- kotlin/01_study/study01/src/main/kotlin/Main.kt
+++ kotlin/01_study/study01/src/main/kotlin/Main.kt
@@ -24,7 +24,16 @@ fun main(args: Array<String>) {
     val s1 = "a is $a calc=${allsum(9,4,3,2)}";
     println(s1);
 
+//类型后面加?表示可为空
+    var age: String? = "23"
+//抛出空指针异常
+    val ages = age!!.toInt()
+//不做处理返回 null
+    val ages1 = age?.toInt()
+//age为空返回-1
+    val ages2 = age?.toInt() ?: -1
 
+    println("$age,$ages1,$ages2, $ages")
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     // println("Program arguments: ${args.joinToString()}")
