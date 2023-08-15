a3ee4385b37290f1efee899eabc67d999cafc8a7
zdhsoft <zdhsoft@163.com>
2023-08-15 17:19:23 +0800

    step simple

--- kotlin/01_study/study01/src/main/kotlin/Main.kt
+++ kotlin/01_study/study01/src/main/kotlin/Main.kt
@@ -59,7 +59,26 @@ fun main(args: Array<String>) {
     for (i in 1..4) {
         print(i)
     }
+    xxmain();
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     // println("Program arguments: ${args.joinToString()}")
+}
+
+fun xxmain() {
+    print("循环输出：")
+    for (i in 1..4) print(i) // 输出“1234”
+    println("\n----------------")
+    print("设置步长：")
+    for (i in 1..4 step 2) print(i) // 输出“13”
+    println("\n----------------")
+    print("使用 downTo：")
+    for (i in 4 downTo 1 step 2) print(i) // 输出“42”
+    println("\n----------------")
+    print("使用 until：")
+    // 使用 until 函数排除结束元素
+    for (i in 1 until 4) {   // i in [1, 4) 排除了 4
+        print(i)
+    }
+    println("\n----------------")
 }
