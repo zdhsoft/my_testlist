623b2463eb12809b5e20376f2a7273a9c18dda9c
zdhsoft <zdhsoft@163.com>
2023-08-17 14:09:25 +0800

    常量

--- java/01_study/study/src/Main.java
+++ java/01_study/study/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        final double CM_PER_INCH = 2.54;
+        double paperWidth = 8.5;
+        double peperHeight = 11;
+        System.out.println("Paper size in centimeters:" + paperWidth * CM_PER_INCH + " by " + peperHeight * CM_PER_INCH);
     }
 }
