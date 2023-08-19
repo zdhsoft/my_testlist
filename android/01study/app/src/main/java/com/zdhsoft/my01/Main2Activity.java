a346c2d34dd8db65bff065464eade0c0ffc6688d
zdhsoft <zdhsoft@163.com>
2023-08-19 11:10:55 +0800

    增加页面3

--- android/01study/app/src/main/java/com/zdhsoft/my01/Main2Activity.java
+++ android/01study/app/src/main/java/com/zdhsoft/my01/Main2Activity.java
@@ -1,4 +1,13 @@
 package com.zdhsoft.my01;
 
-public class Main2Activity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
 }
