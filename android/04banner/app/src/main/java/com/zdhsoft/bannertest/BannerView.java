14da4cad6ab760cae4674b4dfd2df3ea98c776c5
zdhsoft <zdhsoft@163.com>
2023-08-22 22:00:14 +0800

    增加点

--- android/04banner/app/src/main/java/com/zdhsoft/bannertest/BannerView.java
+++ android/04banner/app/src/main/java/com/zdhsoft/bannertest/BannerView.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import androidx.viewpager.widget.ViewPager;
@@ -17,13 +18,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BannerView extends RelativeLayout {
-    private static final int ITEM_COUNT = 3;
+    private static final int ITEM_COUNT = 5;
     private Context context;
 
     private ViewPager viewPager;
     private List<View> viewList;
     private List<Integer> imgResourceList;
-    private RotationHandler routationHandler;
+    private RotationHandler rotationHandler;
+    private LinearLayout dotLayout;    //水平线性布局
 
     public BannerView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -33,6 +35,37 @@ public class BannerView extends RelativeLayout {
 
     private void init() {
         this.initViewPager();
+        this.initDot();
+    }
+
+    private void initDot() {
+        dotLayout = new LinearLayout(context);
+        dotLayout.setOrientation(LinearLayout.HORIZONTAL);   //水平布局
+        //添加圆点
+        for (int i=0; i<ITEM_COUNT; i++){
+            ImageView dotImage = new ImageView(context);
+            if (i == 0){
+                dotImage.setImageResource(R.drawable.dot_white);
+            } else {
+                dotImage.setImageResource(R.drawable.dot_gray);
+            }
+
+            //为ImageView创建布局参数
+            MarginLayoutParams layoutParams = new MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            //设置边距
+            layoutParams.leftMargin = 10;
+            layoutParams.rightMargin = 10;
+            //动态
+            dotLayout.addView(dotImage, layoutParams);
+        }
+
+        //为dotLayout设置布局参数
+        LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.bottomMargin = 30;
+        layoutParams.addRule(CENTER_HORIZONTAL);
+        layoutParams.addRule(ALIGN_PARENT_BOTTOM);
+        addView(dotLayout, layoutParams);
     }
 
     private void initViewPager() {
@@ -66,11 +99,18 @@ public class BannerView extends RelativeLayout {
 
             @Override
             public void onPageSelected(int position) {
-                Log.d("hahaha", "position = " + position);
-                if (position == viewList.size()-1){
-                    viewPager.setCurrentItem(1, false);
-                } else if (position == 0){
-                    viewPager.setCurrentItem(viewList.size() - 2, false);
+                Log.d("hahaha", "position = " + position + " size = " + viewList.size());
+                int currIdx = -1;
+                if (position > viewList.size()){
+                    // viewPager.setCurrentItem(1, false);
+                    currIdx = 1;
+                } else if (position < 0){
+                    // viewPager.setCurrentItem(viewList.size() - 1, false);
+                    currIdx = viewList.size();
+                }
+                Log.d("hahaha", "position = " + position + " size = " + viewList.size() + " currIdx = " + currIdx);
+                if (currIdx != -1) {
+                    viewPager.setCurrentItem(currIdx, false);
                 }
             }
 
@@ -80,8 +120,8 @@ public class BannerView extends RelativeLayout {
             }
         });
 
-        routationHandler = new RotationHandler(this);
-        routationHandler.sendEmptyMessageDelayed(1, 2000);
+        rotationHandler = new RotationHandler(this);
+        rotationHandler.sendEmptyMessageDelayed(1, 2000);
 
     }
 
@@ -97,7 +137,13 @@ public class BannerView extends RelativeLayout {
             super.handleMessage(msg);
             sendEmptyMessageDelayed(1, 2000);
             BannerView bannerView = bannerReference.get();
-            bannerView.viewPager.setCurrentItem(bannerView.viewPager.getCurrentItem() + 1);
+            // bannerView.viewPager.setCurrentItem(bannerView.viewPager.getCurrentItem() + 1);
         }
     }
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        rotationHandler.removeCallbacksAndMessages(null);
+    }
+
 }
