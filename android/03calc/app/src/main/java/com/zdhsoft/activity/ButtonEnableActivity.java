6de979d822e20bdc9a6898e08cca178725bb9e0b
zdhsoft <zdhsoft@163.com>
2023-08-23 13:28:27 +0800

    完成第三章节的例子

---
+++ android/03calc/app/src/main/java/com/zdhsoft/activity/ButtonEnableActivity.java
@@ -0,0 +1,16 @@
+package com.zdhsoft.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zdhsoft.calc.R;
+
+public class ButtonEnableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_enable);
+    }
+}
---
+++ android/03calc/app/src/main/java/com/zdhsoft/activity/ImageButtonActivity.java
@@ -0,0 +1,16 @@
+package com.zdhsoft.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zdhsoft.calc.R;
+
+public class ImageButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_button);
+    }
+}
