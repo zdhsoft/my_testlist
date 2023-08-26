8f3440c6c05e9cee0d894e4e7ed6649ad37ad339
zdhsoft <zdhsoft@163.com>
2023-08-26 15:28:25 +0800

    finish banner demo

--- android/openbanner3/app/src/main/java/com/zdhsoft/openbanner3/MainActivity.java
+++ android/openbanner3/app/src/main/java/com/zdhsoft/openbanner3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.youth.banner.Banner;
 import com.youth.banner.adapter.BannerImageAdapter;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         banner = findViewById(R.id.banner);
         banner.addBannerLifecycleObserver(this);
+        useBanner();
     }
     @Override
     protected void onStart() {
