2b4316582d4b5562d5382024765aa4a1828aee16
zdhsoft <zdhsoft@163.com>
2023-08-26 15:16:01 +0800

    reset again!

--- android/openbanner3/app/build.gradle.kts
+++ android/openbanner3/app/build.gradle.kts
@@ -29,10 +29,14 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("io.github.youth5201314:banner:2.2.2")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.jakewharton:butterknife:10.2.3")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- android/openbanner3/app/src/main/java/com/zdhsoft/openbanner3/MainActivity.java
+++ android/openbanner3/app/src/main/java/com/zdhsoft/openbanner3/MainActivity.java
@@ -4,11 +4,64 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.youth.banner.Banner;
+import com.youth.banner.adapter.BannerImageAdapter;
+import com.youth.banner.holder.BannerImageHolder;
+import com.youth.banner.indicator.CircleIndicator;
+import com.zdhsoft.openbanner3.bean.DataBean;
+
 public class MainActivity extends AppCompatActivity {
 
+    Banner banner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        banner = findViewById(R.id.banner);
+        banner.addBannerLifecycleObserver(this);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //开始轮播
+        banner.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //停止轮播
+        banner.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //销毁
+        banner.destroy();
+    }
+    public void useBanner() {
+//        //--------------------------简单使用-------------------------------
+//        banner.addBannerLifecycleObserver(this)//添加生命周期观察者
+//                .setAdapter(new BannerExampleAdapter(DataBean.getTestData()))
+//                .setIndicator(new CircleIndicator(this));
+
+        //—————————————————————————如果你想偷懒，而又只是图片轮播————————————————————————
+        banner.setAdapter(new BannerImageAdapter<DataBean>(DataBean.getTestData3()) {
+                    @Override
+                    public void onBindView(BannerImageHolder holder, DataBean data, int position, int size) {
+                        //图片加载自己实现
+                        Glide.with(holder.itemView)
+                                .load(data.imageUrl)
+                                .apply(RequestOptions.bitmapTransform(new RoundedCorners(30)))
+                                .into(holder.imageView);
+                    }
+                })
+                .addBannerLifecycleObserver(this)//添加生命周期观察者
+                .setIndicator(new CircleIndicator(this));
+        //更多使用方法仔细阅读文档，或者查看demo
     }
 }
