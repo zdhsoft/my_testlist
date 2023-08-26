f18b45352699ff9ff098b7f1c6925512380cb994
zdhsoft <zdhsoft@163.com>
2023-08-26 11:00:32 +0800

    直接引入源代码

--- android/openbanner2/app/build.gradle.kts
+++ android/openbanner2/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
