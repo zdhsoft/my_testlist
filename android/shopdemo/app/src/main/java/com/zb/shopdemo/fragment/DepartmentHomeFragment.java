10b2d98e951c79dfc043d7f964e64581544e99ee
zdhsoft <zdhsoft@163.com>
2023-08-31 00:03:35 +0800

    增加BannerPager

--- android/shopdemo/app/src/main/java/com/zb/shopdemo/fragment/DepartmentHomeFragment.java
+++ android/shopdemo/app/src/main/java/com/zb/shopdemo/fragment/DepartmentHomeFragment.java
@@ -2,5 +2,7 @@ package com.zb.shopdemo.fragment;
 
 import androidx.fragment.app.Fragment;
 
+import com.zb.shopdemo.widget.BannerPager;
+
 public class DepartmentHomeFragment extends Fragment implements BannerPager.BannerClickListener {
 }
