f0eff98a2c2cb318e9ddf4be868647a8f1a50db3
zdhsoft <zdhsoft@163.com>
2023-08-30 23:07:46 +0800

    add main style

---
+++ android/shopdemo/app/src/main/java/com/zb/shopdemo/DepartmentStoreActivity.java
@@ -0,0 +1,14 @@
+package com.zb.shopdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DepartmentStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_department_store);
+    }
+}
