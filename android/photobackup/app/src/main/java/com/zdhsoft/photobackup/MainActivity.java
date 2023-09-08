d0d6a48bba8a84bc3f9def4701add6756122ab2c
zdhsoft <zdhsoft@163.com>
2023-09-08 23:14:25 +0800

    编写例子

--- android/photobackup/app/src/main/java/com/zdhsoft/photobackup/MainActivity.java
+++ android/photobackup/app/src/main/java/com/zdhsoft/photobackup/MainActivity.java
@@ -1,14 +1,44 @@
 package com.zdhsoft.photobackup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final static String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Log.i(TAG, "onClick!!!");
+        OkHttpClient c = new OkHttpClient();
+        Request r = new Request.Builder().url("https://test.zdhsoft.com.cn/toc/v1/banner_list").build();
+        Call call = c.newCall(r);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.i(TAG, "调用失败!");
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                Log.i(TAG, response.body().string());
+            }
+        });
+
     }
 }
