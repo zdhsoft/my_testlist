d92603a5b3530121758edb82076485b1b0bf7a5f
zdhsoft <zdhsoft@163.com>
2023-09-14 06:59:27 +0800

    编写函数

--- go/src/01_helloworld/json.go
+++ go/src/01_helloworld/json.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
-func testjson() {
-	var s = "[\"a\",2,3,4,5,6]"
-	var 
-	var n []int32
-	err := json.Unmarshal([]byte(s), &n)
+func score2Array(scores string) {
 
-	fmt.Println(err, n, s)
+}
+
+func utils_football_score(home_scores string, away_scores string) (error, []int32, []int32) {
+	var h, a []int32
+	errhome := json.Unmarshal([]byte(home_scores), &h)
+	if errhome != nil {
+		return errhome, h, a
+	}
+
+	erraway := json.Unmarshal([]byte(away_scores), &a)
+	if erraway != nil {
+		return erraway, h, a
+	}
+
+	return nil, h, a
+
+}
+
+func testjson() {
+	// var s = "[1,2,3,4,5,6]"
+	// var h = "[2,4,5,6,7,3]"
+	// var n []int32
+	// err := json.Unmarshal([]byte(s), &n)
+
+	// fmt.Println(err, n, s)
 }
