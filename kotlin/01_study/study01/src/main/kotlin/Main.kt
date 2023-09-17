20e8f744b913f32aa895ab93070179156e555914
zdhsoft <zdhsoft@163.com>
2023-09-17 22:59:09 +0800

    good night

--- kotlin/01_study/study01/src/main/kotlin/Main.kt
+++ kotlin/01_study/study01/src/main/kotlin/Main.kt
@@ -21,7 +21,17 @@ fun main(args: Array<String>) {
 
     listOf(1,2,3,4,5).forEach { x -> println(x) }
 
+    println("value MON :${t(Day.MON)}")
+    println("value TUE :${t(Day.TUE)}")
+    println("value WEN :${t(Day.WEN)}")
+    println("value THU :${t(Day.THU)}")
+    println("value FRI :${t(Day.FRI)}")
+    println("value SAT :${t(Day.SAT)}")
+    println("value SUN :${t(Day.SUN)}")
 
+    for(i: Int in 1..10) {
+        println(i*i+1)
+    }
 }
 
 fun sum(x: Int, y: Int) = x + y;
@@ -42,4 +52,24 @@ fun testType(): Int {
     println(d2.javaClass.name)
 
     return i+100;
+}
+
+enum class Day {
+    MON,
+    TUE,
+    WEN,
+    THU,
+    FRI,
+    SAT,
+    SUN
+
+}
+
+fun t(d: Day): Int {
+    when(d) {
+        Day.FRI -> return 5;
+        Day.MON -> return 4;
+        Day.TUE, Day.WEN -> return 9;
+        else -> return 99;
+    }
 }
