6121bab39960205151ffed678a261b3c1e1a2b74
zdhsoft <zdhsoft@163.com>
2023-09-17 22:13:06 +0800

    基本数据类型

--- kotlin/01_study/study01/src/main/kotlin/Main.kt
+++ kotlin/01_study/study01/src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
 fun main() {
-    System.out.println("hello world");
+    //System.out.println("hello world");
+    var k = testType();
+    System.out.println(k.javaClass.name);
+}
+
+fun testType(): Int {
+    val s = "我是一个字符串";
+    var i = 1314;
+    var l = 1413L;
+    var f = 12.09f;
+    var d = 13.9;
+    var d2 = 10.1e6;
+
+    System.out.println(s.javaClass.name)
+    System.out.println(i.javaClass.name)
+    System.out.println(l.javaClass.name)
+    System.out.println(f.javaClass.name)
+    System.out.println(d.javaClass.name)
+    System.out.println(d2.javaClass.name)
+
+    return i+100;
 }
