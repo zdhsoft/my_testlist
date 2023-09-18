f7457ac49306945b62efba7b8529a4c105de89c8
zdhsoft <zdhsoft@163.com>
2023-09-18 22:25:08 +0800

    增加伴生object

--- kotlin/01_study/study01/src/main/kotlin/study.kt
+++ kotlin/01_study/study01/src/main/kotlin/study.kt
@@ -1,1 +1,9 @@
-data class Study(var age:Int, var name:String, var classNames: String)
+class Study(private var age:Int, private var name:String,private var classNames: String) {
+    companion object {
+        val ONE_LEVEL = 7
+
+        fun isOne(s: Study): Boolean {
+            return s.age == ONE_LEVEL
+        }
+    }
+}
