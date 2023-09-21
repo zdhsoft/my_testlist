b1e70f6094abeac14dabfcfe1765f8f1ec624fca
zdhsoft <zdhsoft@163.com>
2023-09-21 18:32:07 +0800

    完成代码

--- go2/simple/search/default.go
+++ go2/simple/search/default.go
@@ -1,1 +1,14 @@
 package search
+
+type defaultMatcher struct {
+	//
+}
+
+func init() {
+	var matcher defaultMatcher
+	Register("default", matcher)
+}
+
+func (m defaultMatcher) Search(feed *Feed, searchTeam string) ([]*Result, error) {
+	return nil, nil
+}
--- go2/simple/search/search.go
+++ go2/simple/search/search.go
@@ -1,28 +1,51 @@
 package search
 
 import (
-	"go2/simple/matchers"
 	"log"
 	"sync"
 )
 
-func Run(name string) {
-	//
+var matchers = make(map[string]Matcher)
+
+func Run(searchTerm string) {
+	// //
 	feeds, err := RetrieveFeeds()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	results := make(clan * Result)
+
 	var waitGroup sync.WaitGroup
 
 	waitGroup.Add(len(feeds))
+
 	for _, feed := range feeds {
-		matcher, exists:= matchers[feed.Type]
-		if !exists {
+		matcher, exists := matchers[feed.Type]
+		if exists {
 			matcher = matchers["default"]
-			
 		}
 
+		go func(matcher Matcher, feed *Feed) {
+			Match(matcher, feed, searchTerm, results)
+			waitGroup.Done()
+		}(matcher, feed)
 	}
+
+	go func() {
+		waitGroup.Wait()
+		close(results)
+	}()
+	Display(results)
+}
+
+func Register(feedType string, matcher Matcher) {
+	_, exists := matchers[feedType]
+
+	if exists {
+		log.Fatalln(feedType, "Matcher already registered!")
+	}
+
+	log.Println("Register", feedType, "matcher")
+	matchers[feedType] = matcher
 }
