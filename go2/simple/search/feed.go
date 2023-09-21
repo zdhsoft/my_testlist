e04b79445e249f17832c5f394d72a82fee67a2d5
zdhsoft <zdhsoft@163.com>
2023-09-21 18:08:55 +0800

    增加示例代码

--- go2/simple/search/feed.go
+++ go2/simple/search/feed.go
@@ -1,1 +1,30 @@
 package search
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const dataFile = "data/data.json"
+
+type Feed struct {
+	Name string `json:"site"`
+	URI  string `json:"link"`
+	Type string `json:"type"`
+}
+
+func RetrieveFeeds() ([]*Feed, error) {
+	file, err := os.Open(dataFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var feeds []*Feed
+	err = json.NewDecoder(file).Decode(&feeds)
+	if err != nil {
+		return nil, err
+	} else {
+		return feeds, nil
+	}
+}
--- go2/simple/search/search.go
+++ go2/simple/search/search.go
@@ -1,5 +1,28 @@
 package search
 
+import (
+	"go2/simple/matchers"
+	"log"
+	"sync"
+)
+
 func Run(name string) {
 	//
+	feeds, err := RetrieveFeeds()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results := make(clan * Result)
+	var waitGroup sync.WaitGroup
+
+	waitGroup.Add(len(feeds))
+	for _, feed := range feeds {
+		matcher, exists:= matchers[feed.Type]
+		if !exists {
+			matcher = matchers["default"]
+			
+		}
+
+	}
 }
