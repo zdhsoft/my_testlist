8e230e0837bcd182ed3f8906a57ba43c2ae1eb12
zdhsoft <zdhsoft@163.com>
2023-09-30 23:25:20 +0800

    sha256示例代码

--- typescript/29_cleantable/cleantable/src/main.ts
+++ typescript/29_cleantable/cleantable/src/main.ts
@@ -2,18 +2,33 @@ import { DBTools } from './dbtools';
 import { getLogger } from 'xmcommon';
 import { cleanTable } from './cleanTable';
 import { XMCurrency } from 'xmcurrency';
+import crypto from 'crypto';
 const log = getLogger(__filename);
-
+// const hmac = crypto.createHmac("sha256", this.key);
+// return hmac.update(contentToBeSigned).digest('base64');
 function test() {
-    const sumAmountIncludingTax = '130000.00';
-    const sumExcludingTaxAmount = '122641.51';
-
-    const c1 = new XMCurrency(sumAmountIncludingTax);
-    const c3 = c1.sub(sumExcludingTaxAmount);
-    console.log(c1.format(), c3.format());
+    const token = 'xxxxyyyy';
+    const t = 1669872112;
+    const hash = crypto.createHmac('sha256', token);
+    const h = crypto.createHash('sha256');
+    const mm = hash.update(t.toString()).digest('hex');
+    const m = h.update(`${token}${t}`).digest('hex');
+    log.info(mm);
+    log.info(m);
 }
 
 test();
+
+// function test() {
+//     const sumAmountIncludingTax = '130000.00';
+//     const sumExcludingTaxAmount = '122641.51';
+
+//     const c1 = new XMCurrency(sumAmountIncludingTax);
+//     const c3 = c1.sub(sumExcludingTaxAmount);
+//     console.log(c1.format(), c3.format());
+// }
+
+// test();
 
 // spec_elec
 // :
@@ -28,23 +43,23 @@ test();
 // :
 // "122641.51"
 
-async function main() {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    // const cleanTable = require('../cleanTable.json');
+// async function main() {
+//     // eslint-disable-next-line @typescript-eslint/no-var-requires
+//     // const cleanTable = require('../cleanTable.json');
 
-    log.log(JSON.stringify(cleanTable));
-    log.log('hello world!');
+//     log.log(JSON.stringify(cleanTable));
+//     log.log('hello world!');
 
-    const list = cleanTable;
-    if (Array.isArray(list) && list.length > 0) {
-        const cnt = list.length;
-        for (let i = 0; i < cnt; i++) {
-            await DBTools.cleanTable(list[i], i + 1, cnt);
-        }
-    }
-    log.info(`处理完成!`);
-    process.exit(0);
-}
+//     const list = cleanTable;
+//     if (Array.isArray(list) && list.length > 0) {
+//         const cnt = list.length;
+//         for (let i = 0; i < cnt; i++) {
+//             await DBTools.cleanTable(list[i], i + 1, cnt);
+//         }
+//     }
+//     log.info(`处理完成!`);
+//     process.exit(0);
+// }
 // main();
 
 // delete from avenger_approvalloan;
