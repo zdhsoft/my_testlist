17b02fc9bdf5472a8901ac9303d28778804689f8
zdhsoft <zdhsoft@163.com>
2023-10-02 11:01:46 +0800

    创建工程。

--- typescript/imclient/src/main.ts
+++ typescript/imclient/src/main.ts
@@ -1,14 +1,5 @@
-/*************************************************************************
- * Copyright(c) 2017-2022,
- *
- * All rights reserved.
- *
- * @filename main.ts
- * @summary：项目主程序
- * @version: 1.0
- * ------------------------------------------------------------------------
- * version             author            reason             date
- * 1.0                 zdhsoft      创建文件            2023-10-02
- *************************************************************************/
+import sdk from 'tencentcloud-sdk-nodejs';
+const ImClient = sdk.cim.v20190318.Client;
 
+  // tencentcloud.im.v20190318.Client;
 console.log('hello world!');
