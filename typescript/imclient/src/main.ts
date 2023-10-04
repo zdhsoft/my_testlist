548eb3f30282a74bfb4d589c27b00d2fe93afdb2
zdhsoft <zdhsoft@163.com>
2023-10-04 11:30:56 +0800

    更新一版配置、

--- typescript/imclient/src/main.ts
+++ typescript/imclient/src/main.ts
@@ -1,13 +1,15 @@
-import sdk from 'tencentcloud-sdk-nodejs';
+import * as tencentcloud from 'tencentcloud-sdk-nodejs';
 import { clientConfig } from './config';
-const ImClient = sdk.cim.v20190318.Client;
 
 // tencentcloud.im.v20190318.Client;
 // console.log('hello world!');
 
 async function main() {
-    const client = new ImClient(clientConfig);
+    const t = tencentcloud;
 
+    const ImClient = t.cim.v20190318.Client;
+
+    const client = new ImClient(clientConfig);
     const params = {};
 
     const data = await client.DescribeSdkAppid(null, (err, desp) => {
