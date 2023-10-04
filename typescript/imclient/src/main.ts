721a3e44c39e20d2297ff16200032f7b8e758792
zdhsoft <zdhsoft@163.com>
2023-10-04 11:08:50 +0800

    增加im测试代码

--- typescript/imclient/src/main.ts
+++ typescript/imclient/src/main.ts
@@ -1,5 +1,20 @@
 import sdk from 'tencentcloud-sdk-nodejs';
+import { clientConfig } from './config';
 const ImClient = sdk.cim.v20190318.Client;
 
-  // tencentcloud.im.v20190318.Client;
-console.log('hello world!');
+// tencentcloud.im.v20190318.Client;
+// console.log('hello world!');
+
+async function main() {
+    const client = new ImClient(clientConfig);
+
+    const params = {};
+
+    const data = await client.DescribeSdkAppid(null, (err, desp) => {
+        console.log(err, desp);
+    });
+
+    console.log(data);
+}
+
+main();
