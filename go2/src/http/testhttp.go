316eeb2a7e0c9f8b756ec16deb40379284a8171f
zdhsoft <zdhsoft@163.com>
2023-10-15 21:19:45 +0800

    实现http请求。

--- go2/src/http/testhttp.go
+++ go2/src/http/testhttp.go
@@ -1,7 +1,18 @@
 package testhttp
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func TestHttp() {
 	fmt.Println("hello test Http!")
+
+	resp, err := http.Get("https://www.baidu.com/")
+	errmsg := ""
+	if err != nil {
+		errmsg = err.Error()
+	}
+
+	fmt.Printf("resp:%+v\n, err:%s", resp, errmsg)
 }
