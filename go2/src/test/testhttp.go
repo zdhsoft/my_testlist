c2a248ae35484cd58131b4aa7f196bcbe9838758
zdhsoft <zdhsoft@163.com>
2023-10-23 22:27:16 +0800

    实现简单的GET

--- go2/src/test/testhttp.go
+++ go2/src/test/testhttp.go
@@ -1,7 +1,23 @@
 package test
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func TestHttp() {
 	fmt.Println("Test Http!")
+	resp, err := http.Get("http://httpbin.org/get")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println("Body:" + string(body))
+	fmt.Printf("status: %d\n", (resp.StatusCode))
+	if resp.StatusCode == 200 {
+		fmt.Println("ok")
+	}
 }
