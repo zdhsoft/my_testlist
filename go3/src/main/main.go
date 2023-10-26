e2c805e980ee8f734c1db5e38d788f5a76e4eea5
zdhsoft <zdhsoft@163.com>
2023-10-26 22:58:19 +0800

    清理代码

--- go3/src/main/main.go
+++ go3/src/main/main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
 func main() {
-	fmt.Println("hello world! this is go3 study!")
-	httpMain()
-}
 
-func httpMain() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/count", counter)
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
-
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("hander::URL.Path=%q\n", r.URL.Path)
-	fmt.Fprintf(w, "URL.Path=%q\n", r.URL.Path)
-}
-
-func counter(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("counter::URL.Path=%q\n", r.URL.Path)
-	fmt.Fprintf(w, "URL.Path=%q\n", r.URL.Path)
 }
