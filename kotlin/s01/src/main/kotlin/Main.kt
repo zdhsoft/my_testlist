c27bdd2d647bb2cd4489ea43a7d0524f178df59a
zdhsoft <zdhsoft@163.com>
2023-12-13 20:41:27 +0800

    增加学习simple

--- kotlin/s01/src/main/kotlin/Main.kt
+++ kotlin/s01/src/main/kotlin/Main.kt
@@ -8,5 +8,10 @@ fun main(args: Array<String>) {
     println("------------->")
 
     println(" 1 + 2 =  " + my.demo.sum(1, 2))
-    my.demo.PrintSum()
+    my.demo.printSum()
+    my.demo.printSum2(99,100)
+    my.demo.simple()
+
+    my.demo.testX()
+    println("pi =" + my.demo.PI)
 }
--- kotlin/s01/src/main/kotlin/my/demo/source.kt
+++ kotlin/s01/src/main/kotlin/my/demo/source.kt
@@ -1,12 +1,47 @@
 package my.demo
 
+val PI = 3.1415926
+var xxx = 0
+
 fun sum(a: Int, b: Int): Int {
     return a + b
+}
+
+fun incX() {
+    xxx++
+}
+
+fun getX():Int {
+    return xxx
+}
+
+fun testX() {
+    for (i in 1..10) {
+        println("xxx = " + getX())
+        incX()
+    }
 }
 
 fun sum1(a: Int, b:Int) = a + b
 
 
-fun PrintSum(): Unit {
+fun printSum() {
     println("sum1:1+2=" + sum1(1,2))
+}
+
+fun printSum2(a: Int, b: Int) {
+    println("sum of $a and $b is ${a+b}")
+}
+
+fun simple() {
+    val a = 2
+    val b = 2
+    val c = 3
+    println("a=$a, b=$b, c=$c" + sss(999))
+}
+
+fun sss(a: Int) : Int {
+    var x = 5
+    x += 5
+    return a + x
 }
