d135f02f12c0108fb2eb4601f45312345d601636
zdhsoft <zdhsoft@163.com>
2023-12-13 17:45:37 +0800

    删除临时文件

--- kotlin/01_study/study01/src/main/kotlin/prize.kt
+++
@@ -1,9 +0,0 @@
-class prize(private val name: String, private val count: Int, private val type: Int) {
-    companion object {
-        var TYPE_REDPACK = 0
-        var TYPE_COUPON = 1
-        fun isRedpack(p: prize): Boolean {
-            return p.type == TYPE_REDPACK
-        }
-    }
-}
--- kotlin/01_study/study01/src/main/kotlin/study.kt
+++
@@ -1,9 +0,0 @@
-class Study(private var age:Int, private var name:String,private var classNames: String) {
-    companion object {
-        val ONE_LEVEL = 7
-
-        fun isOne(s: Study): Boolean {
-            return s.age == ONE_LEVEL
-        }
-    }
-}
