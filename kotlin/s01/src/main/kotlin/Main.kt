bc4afd9894502874f44b9351303fb3766b860ac2
zdhsoft <zdhsoft@163.com>
2023-12-13 19:53:55 +0800

    开始学习示例

---
+++ kotlin/s01/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println("Program arguments: ${args.joinToString()}")
+
+
+    print("hello ")
+    println("World!")
+    println("------------->")
+
+    println(" 1 + 2 =  " + my.demo.sum(1, 2))
+    my.demo.PrintSum()
+}
---
+++ kotlin/s01/src/main/kotlin/my/demo/source.kt
@@ -0,0 +1,12 @@
+package my.demo
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun sum1(a: Int, b:Int) = a + b
+
+
+fun PrintSum(): Unit {
+    println("sum1:1+2=" + sum1(1,2))
+}
