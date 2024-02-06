91f6b3cbf4550f8e02a8adb83cffd1373d6905f3
zdhsoft <zdhsoft@163.com>
2024-02-06 15:06:28 +0800

    add simple

--- ggg/g1/go.mod
+++ ggg/g1/go.mod
@@ -1,3 +1,7 @@
 module g1
 
-go 1.21
+go 1.21.1
+
+toolchain go1.21.5
+
+require github.com/zdhsoft/xmutilsgo v0.1.0 // indirect
--- ggg/g1/src/main/main.go
+++ ggg/g1/src/main/main.go
@@ -1,9 +1,49 @@
 package main
 
-import _chan "g1/src/chan"
+import (
+	"fmt"
+)
+
+func P(prompt string, arr []int) {
+	fmt.Printf("%s => %p\n", prompt, arr)
+	fmt.Print("[")
+	for _, v := range arr {
+		fmt.Printf("%d, ", v)
+	}
+	fmt.Print("]\n")
+}
+
+type S struct {
+	A1 int
+	A2 string
+	A3 int
+}
+
+func newS() *S {
+	return new(S)
+}
+
+func CreateS() *S {
+	return &S{}
+}
 
 func main() {
-	// fmt.Println("hello world!")
-	// _chan.TTT()
-	_chan.K()
+	a := []int{1, 2, 3, 4}
+	P("first", a)
+	b := append(a, 4, 5, 6)
+	a = append(a, 1, 2)
+	P("second a", a)
+	P("second b", b)
+
+	s3 := S{}
+
+	s1 := newS()
+	s2 := CreateS()
+
+	n1 := 1
+	n2 := 2
+	n3 := 3
+
+	fmt.Printf("%p, %p, %p\n", s1, s2, &s3)
+	fmt.Printf("n=== %p, %p, %p\n", &n1, &n2, &n3)
 }
